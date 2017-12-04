a414dfd768ed995eb79366aeccd05fff02b73a4b
Gabriella <gaba@Gabriellas-MacBook-Pro.local>
2017-12-04 15:56:23 -0600

    app 1 complete

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -2,20 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(){
+    super();
+    
+    this.state = {
+      message: ""
+    }
+  }
+
+  handleChange( value ) {
+    this.setState ({ message: value })
+  }
+
+
+
   render() {
-    return (
+      return (
       <div className="App">
+
+      <input onChange={ (e) => this.handleChange( e.target.value ) } type="text"/>
+
+ <p>{ this.state.message }</p>
+
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>m a g i c</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        
       </div>
     );
   }
 }
 
-export default App;
+
